451498be7acd7406ba0d88d1e3ba2c831c1b30a6
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2026-06-17 18:00:47 +0800

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -95,8 +95,8 @@ def get_imagenet_c_sequence(data_dir, severity=5, batch_size=50,
         transforms.Resize(256),
         transforms.CenterCrop(img_size),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5],
-                             std=[0.5, 0.5, 0.5]),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
     ])
 
     loaders = []
